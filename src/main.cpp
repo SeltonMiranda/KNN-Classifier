4499482ad753a26983ea36b3124a61423e79ee9c
Selton <seltonmiranda00@gmail.com>
2024-10-01 22:40:35 -0300

    testing self-training sample

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main() {
 
     //classifier.cropper->makeCrop(classifier.cropper->getFolder());
     classifier.generate_data("./PKLotSegmented/PUCPR", "PUCPR_NORM.csv");
-    classifier.generate_data("./PKLotSegmented/UFPR04", "UFPR04_NORM.csv");
+    //classifier.generate_data("./PKLotSegmented/UFPR04", "UFPR04_NORM.csv");
     //classifier.generate_data("./PKLot/PKLotSegmented/UFPR05", "UFPR05_NORM.csv");
 
     std::vector<std::vector<float>> x_test;
@@ -17,7 +17,8 @@ int main() {
     float accuracy{0};
   
     classifier.extract_data_and_labels("PUCPR_NORM.csv", classifier.x_train, classifier.y_train);
-    classifier.extract_data_and_labels("UFPR04_NORM.csv", x_test, y_test);
+    classifier.extract_data_and_labels("PUCPR_NORM.csv", x_test, y_test);
+    //classifier.extract_data_and_labels("UFPR04_NORM.csv", x_test, y_test);
 
     predicted_labels = classifier.classify(x_test);
     confusion_matrix = classifier.confusion_matrix(predicted_labels, y_test);
